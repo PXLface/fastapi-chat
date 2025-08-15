ecb080a09de932df549faa195aa02b0367938046
PXLface <iipixelfaceii@gmail.com>
2025-08-15 06:15:30 +0300

    Add tests

--- app/domain/entities/messages.py
+++ app/domain/entities/messages.py
@@ -1,11 +1,21 @@
 from dataclasses import dataclass, field
-from uuid import uuid4
+
+from domain.entities.base import BaseEntity
+from domain.values.messages import Title
 
 
-@dataclass
-class Message:
-    oid: str = field(
-        default_factory=lambda: str(uuid4),
+@dataclass(frozen=True)
+class Message(BaseEntity):
+    text: str
+
+
+@dataclass(frozen=True)
+class Chat(BaseEntity):
+    title: Title
+    messages: set[Message] = field(
+        default_factory=set,
         kw_only=True,
     )
-    text: str
+
+    def add_message(self, message: Message):
+        self.messages.add(message)
--- app/domain/exceptions/messages.py
+++ app/domain/exceptions/messages.py
@@ -4,9 +4,16 @@ from domain.exceptions.base import ApplicatedException
 
 
 @dataclass(eq=False)
-class TextTooLongException(ApplicatedException):
+class TitleToolongException(ApplicatedException):
     text: str
 
     @property
     def message(self):
         return f'Слишком длинный текст сообщения "{self.text[:255]}..."'
+
+
+@dataclass(eq=False)
+class EmptyTextException(ApplicatedException):
+    @property
+    def message(self):
+        return 'Текст не может быть пустым'
